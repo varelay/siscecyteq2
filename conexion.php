27d5618a04dfddd2b2fce30c79093ec1fec038b8
varelay <89622998+varelay@users.noreply.github.com>
2021-11-23 19:58:54 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 
 
-    $conexion = new mysqli("localhost","samuel","1234","cecyteq");
+    $conexion = new mysqli("ls-20c21c77c56c2d07648f7a1579f9a4a10b244a6a.cpefgjofumk0.us-east-2.rds.amazonaws.com","dbmasteruser","TiS>>qNh!`Uc;Et[o2W^gX5r}^r,00mi","bdcecyteq");
 
     if($conexion){
     } else {
